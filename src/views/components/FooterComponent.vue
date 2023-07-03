42a82bf20de20b5d99ea8a06dba29ce2ef268a52
MarcoBione <marcobione.work@gmail.com>
2023-07-03 15:52:12 +0200

    Add palette + footer items

--- src/views/components/FooterComponent.vue
+++ src/views/components/FooterComponent.vue
@@ -1,7 +1,59 @@
 <template>
-    <footer>
-        
+    <footer class="d-flex">
 
+            <!-- logo sx -->
+            <div class="_debug _mypos">
+                <img class="_mylogo" src="/images/Logo.png" alt="Hungry-logo">
+            </div>
+            
+            <!-- sezione sx -->
+            <div class="_debug d-flex align-items-center justify-content-between px-3">
+                <!-- lista sx -->
+                <ul class="m-0 px-3">
+
+                    <!-- tab title -->
+                    <li class="list-unstyled m-0 fw-bold">
+                        Servizio clienti
+                    </li>
+
+                    <!-- Lista di links -->
+                    <li class="list-unstyled m-0">
+                        <a href="#">Domande frequenti</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Accedi</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Registrati</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Scarica l'app</a>
+                    </li>
+                </ul>
+                <!-- lista dx -->
+                <ul class="m-0 px-3">
+
+                    <!-- tab title -->
+                    <li class="list-unstyled m-0 fw-bold">
+                        Chi siamo
+                    </li>
+
+                    <!-- Lista di links -->
+                    <li class="list-unstyled m-0">
+                        <a href="#">Info su "Hungry?"</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Termini e condizioni</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Informativa sulla privacy</a>
+                    </li>
+                    <li class="list-unstyled m-0">
+                        <a href="#">Cookie Policy</a>
+                    </li>
+                </ul>
+
+            </div>
     </footer>
 </template>
 
@@ -12,5 +64,22 @@
 </script>
 
 <style lang="scss" scoped>
+
+@use '../../assets/partials/variables' as*;
+
+// dimensione logo
+._mylogo{
+    width: 300px;
+}
+
+//rimozione dello stile di default per i links + hover
+a{
+    text-decoration: none;
+    color: $carbon;
+}
+a:hover{
+    transition: color 0.3s;
+    color: $orange;
+}
 
 </style>
