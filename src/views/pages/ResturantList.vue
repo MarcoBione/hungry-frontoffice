7fd35a5ba16fbfd301ffea2446fda091b646ccef
MarcoBione <marcobione.work@gmail.com>
2023-07-04 16:40:55 +0200

    Add resturantcard

---
+++ src/views/pages/ResturantList.vue
@@ -0,0 +1,26 @@
+<template>
+    <section >
+        <RestaurantCard/>
+    </section>
+</template>
+
+<script>
+import RestaurantCard from '../components/RestaurantCard.vue';
+
+export default {
+    name : "ResturantList",
+    components : {
+        RestaurantCard
+    },
+
+    data () {
+        return {
+
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
