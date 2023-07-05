ea0d6cf3c2ae508d283b0f5f654b83cbe044fbdb
AndreaCollura <andrea.collur@gmail.com>
2023-07-05 10:51:38 +0200

    home page component created

---
+++ src/views/components/CategoriesComponent.vue
@@ -0,0 +1,40 @@
+<template>
+    <section id="categories-home" class="container-fluid ">
+        <div class="row flex-md-row justify-content-md-evenly flex-column align-items-center">
+            <div class="col-12 ">
+                <h1>Di cosa hai voglia?</h1>
+            </div>
+            <CategoryCard />
+        </div>
+        <div class="d-flex flex-column my-5">
+            <a href="/restaurantlist" class="_button text-uppercase">mostra altro</a>
+        </div>
+    </section>
+</template>
+
+<script>
+
+import CategoryCard from '../components/CategoryCard.vue';
+
+export default {
+    name: 'CategoriesComponent',
+    components: {
+        CategoriesComponent,
+        CategoryCard
+    }
+
+
+}
+</script>
+
+<style lang="scss" scoped>
+#categories-home {
+    h1 {
+        font-size: 2.5rem;
+        font-weight: 700;
+        text-align: center;
+
+
+    }
+}
+</style>
