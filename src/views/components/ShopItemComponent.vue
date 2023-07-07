38d333a72e1a824faabc1c75ad4a46c2f3a1df54
MarcoBione <marcobione.work@gmail.com>
2023-07-07 18:39:45 +0200

    Add  control + layout base

--- src/views/components/ShopItemComponent.vue
+++ src/views/components/ShopItemComponent.vue
@@ -2,24 +2,59 @@
     <section class="py-4">
 
         <!-- ### ordini ### -->
-        <div class="border-top border-white py-3">
+        <div class="border-top border-white">
 
             <ul class="list-group bg-transparent">
 
               <li class="border-0 list-group-item bg-transparent d-flex justify-content-between px-0" v-for="(item, index) in storeData" :key="index">
 
-                <div class="py-2 d-flex border-transparent">
+                 <!--<div class="py-2 d-flex border-transparent j">
 
-                    <!-- ### se presenti elementi nella "chiamata" localstorage ### -->
-                    <p class="m-0 text-white" v-if="myCart">{{ item.title }}</p>
-                    <p class="m-0 px-2 text-white" v-if="myCart">{{ item.qta }}</p>
+                    <div class="d-flex">
+                        ### se presenti elementi nella "chiamata" localstorage ### 
+                            
+                        <div>
+                            <label for="title" class="text-white-50">titolo</label>
+                            <p id="title" class="m-0 text-white" >{{ item.title }}</p>
+                        </div>
 
-                </div>
+                        <div>
+                            <label for="quantita" class="text-white-50">quantita'</label>
+                            <p id="quantita" class="m-0 text-white px-2">{{ item.qta }}</p>
+                        </div>
+
+                        
+
+                    </div>
+
+                    ### se presenti elementi nella "chiamata" localstorage ### 
+                    <button class="btn btn-outline-danger">
+                        <i class="fa-solid fa-trash" ></i>
+                    </button>
+
+                </div>-->
                 
-                <!-- ### se presenti elementi nella "chiamata" localstorage ### -->
-                <button class="btn btn-outline-danger" v-if="myCart">
-                    <i class="fa-solid fa-trash" ></i>
-                </button>
+                <table class="table border-0" v-if="myCart">
+                  <thead>
+                    <tr>
+                        <td class="bg-transparent text-white-50" scope="col">Ordine</td>
+                        <td class="bg-transparent text-white-50" scope="col">Qtà'</td>
+                        <td class="bg-transparent text-white-50" scope="col"></td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                        <th class="bg-transparent text-white" scope="row">{{ item.title }}</th>
+                        <td class="bg-transparent text-white">{{ item.qta }}</td>
+                        <td class="bg-transparent text-white">
+                            <button class="btn btn-outline-danger">
+                                <i class="fa-solid fa-trash" ></i>
+                            </button>
+                        </td>
+                    </tr>
+                  </tbody>
+                </table>
+
             </li>
 
             <li class="border-0 list-group-item bg-transparent d-flex justify-content-between px-0"> 
@@ -31,8 +66,8 @@
 
         </div>
 
-        <!-- ### comandi ### -->
-        <div v-if="myCart">
+        <!-- ### comandi ###  v-if="myCart" -->
+         <div v-if="myCart" > 
             <a class="btn btn-success me-1">
                 Conferma
             </a>
