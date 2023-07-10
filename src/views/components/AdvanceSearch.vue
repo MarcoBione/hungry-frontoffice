f846b1f2a9285a97d0269fd36c3ad7c46a11ca37
marikadiblasio <marbla.albram@gmail.com>
2023-07-10 09:25:42 +0200

    add mb to search, removed not used seachbars, add error

--- src/views/components/AdvanceSearch.vue
+++ src/views/components/AdvanceSearch.vue
@@ -1,20 +1,20 @@
 <template>
   <section>
-    <!-- ### nome del ristorante ### -->
+    <!-- ### nome del ristorante ###
     <div class="input-group mb-3">
       <label class="input-group-text" for="name">Nome ristorante</label>
       <input id="name" type="text" class="form-control bg-body-secondary" placeholder="Inserisci il nome del ristorante">
-    </div>
+    </div> -->
 
-    <div class="input-group mb-3">
-      <!-- ### select per le categorie ### -->
+    <!-- <div class="input-group mb-3">
+       ### select per le categorie ###
       <label class="input-group-text" for="select01">Categorie</label>
       <select class="form-select bg-body-secondary" id="select01">
         <option selected>Seleziona</option>
         <option value="#">Aggiungere nome dinamico</option>
       </select>
-    </div>
-    <div class="form-group">
+    </div> -->
+    <div class="form-group mb-3">
       <p>Seleziona le categorie:</p>
       <div class="d-flex flex-wrap">
         <div v-for="item in categories" class="col-6 col-md-4 col-lg-3">
@@ -49,17 +49,24 @@ export default {
           this.categories = res.data.results;
           console.log(this.categories);
         } else {
-          this.error = res.data.results;
+          this.store.error = res.data.results;
         }
       });
     },
     getSelectedCaterer() {
         console.log(this.items);
+        this.store.error = '';
       if(this.items){
-        // axios.get(`${this.apiBaseUrl}/categories/[${this.items}]`).then((res) => {
+        // axios.get(`${this.store.apiBaseUrl}/categories/${this.items}`).then((res) => {
         axios.get(`${this.store.apiBaseUrl}/caterers`, {params: { 'id' : this.items }}).then((res) => {
-        console.log(res.data.results);
-        this.store.caterers = res.data.results;
+          if (res.data.success) {
+            console.log(res.data.results);
+            this.store.caterers = res.data.results;
+          }else{
+            this.store.error = res.data.results;
+          }
+      // }).catch((res)=>{
+      //   this.store.error = res.data.results;
       });
       }else{
         axios.get(`${this.store.apiBaseUrl}/caterers`).then((res) => {
